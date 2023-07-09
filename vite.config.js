b816618b84a5dda7d822b1f9dce330dbdd891938
zachlamont <zachlamont1@gmail.com>
2023-07-09 20:53:40 +1000

    update base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hypochlorite-dosing-calculator/",
+  base: base,
 });
