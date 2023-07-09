4188285cfda082cb612c20cbd742819daa09086d
zachlamont <zachlamont1@gmail.com>
2023-07-09 20:57:04 +1000

    dynamically deterimine base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Determine base dynamically based on environment
+const base = process.env.NODE_ENV === "production" ? "/hypochlorite-dosing-calculator/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
