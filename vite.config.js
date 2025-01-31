14ef52d1f0fb9408f7cbe2540282e65aaeefe7ad
zachlamont <zachlamont1@gmail.com>
2025-01-31 11:12:29 +1000

    Deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // Determine base dynamically based on environment
 //const base =
-  //process.env.NODE_ENV === "production"
-   // ? "/hypochlorite-dosing-calculator/"
-   // : "/";
+//process.env.NODE_ENV === "production"
+// ? "/hypochlorite-dosing-calculator/"
+// : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
